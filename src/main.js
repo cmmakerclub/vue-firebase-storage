fa0e6be7225eef183db334a5bfb7d8182d4dab73
Gr4vity4 <prawared.bw@gmail.com>
2019-08-12 04:10:00 +0700

    add file upload to firebase storage

--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+const firebase = require('firebase');
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
+  data() {
+    return {
+      firebase: firebase
+    }
+  },
   router,
   render: h => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Gallery Storage</h1>
+    <input type="file" @change="fileChange"><br><br>
+    <button type="button" @click="upload">Upload</button>&ensp;<span>{{ percentage }}%</span>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      firebase: this.$root.$data.firebase,
+      file: null,
+      ref: 'photos/',
+      percentage: 0
+    }
   },
+  methods: {
+    upload() {
+      let file = this.file
+      console.log(file)
+      let storageRef = this.firebase.storage().ref(this.ref + file.name)
+      let task = storageRef.put(file)
+      task.on('state_changed',
+        function progress(snapshot) {
+          let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          window.then.percentage = percentage.toFixed(0)
+        },
+        function error(err) {
+          console.log(err)
+        },
+        function complete() {
+          console.log('upload completed')
+        }
+      )
+    },
+    fileChange(e) {
+      this.file = e.target.files[0]
+    }
+  },
+  mounted() {
+    window.then = this
+  }
 };
+
 </script>
